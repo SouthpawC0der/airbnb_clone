1691106fdd07d1dcdeccc908bafa4025335e89c8
southpawcoder <southpaw1224@outlook.com>
2024-05-28 22:28:20 -0400

    Initial Commit

---
+++ airbnb_clone/airbnb_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_cloneApp.swift
+//  airbnb_clone
+//
+//  Created by Christopher Doyle on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
